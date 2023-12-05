d0e84135e23df9e609d57ff1813e0cfdabb4205c
WellingtonDefassio <well.defassio@gmail.com>
2023-12-05 18:23:55 -0300

    add provider and store redux.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import "react-native-gesture-handler";
+import { React } from "react";
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
+import { Provider } from "react-redux";
 import Navigator from "./src/Navigator";
+import storeConfig from "./src/store/storeConfig";
 
-AppRegistry.registerComponent(appName, () => Navigator);
+const store = storeConfig();
+
+function Redux() {
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Redux);
---
+++ src/store/actions/UserAction.ts
@@ -0,0 +1,15 @@
+import { USER_LOGGED_IN, USER_LOGGED_OUT } from "./actionTypes";
+
+
+export function login(user: any) {
+  return {
+    type: USER_LOGGED_IN,
+    payload: user
+  };
+}
+
+export function logout() {
+  return {
+    type: USER_LOGGED_OUT
+  };
+}
